09967757d58e526f102fd3c2fb8dcbd32ac0f5d5
Priyadharshini Murugan <priyadharshini.mu@itp.objectfrontier.com>
2024-02-26 10:35:25 +0530

    first commit

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+using JenkinsDemo;
+using NUnit.Framework.Interfaces;
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_sayJenkins_1()
+        {
+            TestMe obj1=new TestMe();
+            String result = obj1.sayJenkins(2);
+            Assert.AreEqual("JenkinsJenkins", result);
+        }
+
+        public void Test_sayJenkins_2()
+        {
+            TestMe obj1 = new TestMe();
+            String result = obj1.sayJenkins(1);
+            Assert.AreEqual("Jenkins", result);
+        }
+    }
+
+}
